d04761199894899fc1995a2f2eaa24d33c04ac80
Dosubeen630 <subeendo@SuBeenDo.kornet>
2024-06-29 01:36:17 +0900

    ch2 problem 6

---
+++ ch2/CC_and_Cheesekun.py
@@ -0,0 +1,13 @@
+w = int(input())
+c = int(input())
+
+if w == 3 and ((c > 95) or (c == 95)):
+    print("C.C. is absolutely satisfied with her pizza.")
+elif w == 1 and ((c < 50) or (c == 50)):
+    print("C.C. is fairly satisfied with her pizza.")
+else:
+    print("C.C. is very satisfied with her pizza.")
+
+
+
+
