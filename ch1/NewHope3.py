b3ec2c346d0de869f95102680e19fc6c1ba64584
Dosubeen630 <subeendo@SuBeenDo.kornet>
2024-07-22 20:46:25 +0900

    1장 책의 문제 연습문제 3,4,5문제 풀어봄

--- ch1/NewHope3.py
+++ ch1/NewHope3.py
@@ -13,13 +13,14 @@ if not far_num.isdigit(): exit(f"{far_num} != digit")
 far_num = int(far_num)
 if not 1 <= far_num <= 5: exit(f"{far_num}의 범위에 벗어 남")
 
+count = far_num-1
 if far_num != 1:
     # result = "A long time ago in a galaxy" + " " + ("far," + " " * far_num) + " " + "away..."
 # far 라는 단어를 입력 받은 숫자 만큼 곱하여 출력해 준다. 문제는 1일때는 쉼표 없음. 나머지의 경우 쉼표 추가 해줘야함.
 # 위의 경우 에는 far 라는 단어와 단어 사이에 공백을 주고자 추가 하였 더니 앞의 far만 출력 되고 나머지는 출력 안됨.
      # result = "A long time ago in a galaxy" + " " + ("far, " * far_num) + " " + "away..."
     # 위의 경우에는 far 단어 뒤에 공백을 추가만 해줌. 입력된 수 만큼 far 단어는 출력 잘 됨. 문제는 마지막 단어에는 쉼표가 출력되면 안됨.
-    count = far_num-1
+
     result = "A long time ago in a galaxy" + " " + ("far, " * count ) + "far" + " " + "away..."
     # 유추 해보 자면, far 단어가 한번 있을 때도 쉼표가 출력이 안되게 해야 하니 그걸 응용 해야 할듯!
 
@@ -27,3 +28,9 @@ if far_num != 1:
 else: # 1일 경우
     result = "A long time ago in a galaxy" + " " + "far" + " " + "away..."
     print(result)
+
+# join 함수 사용 해봄.
+a = "far, " * count
+b = ["A long time ago in a galaxy ", a ,"far"," away..." ]
+result = "" .join(b)
+print(result)
