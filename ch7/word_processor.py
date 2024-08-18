53128a400b0e22afbdd90fafbe485cc1b750931c
Dosubeen630 <subeendo@SuBeenDo.kornet>
2024-08-18 23:15:37 +0900

    6장 연습문제 5번

---
+++ ch7/word_processor.py
@@ -0,0 +1,26 @@
+# https://usaco.org/index.php?page=viewproblem2&cpid=987
+
+input_file = open('word.in','r')
+output_file = open('word.out', 'w')
+
+lst = input_file.readline().split()
+n = int(input())
+k = int(input())
+words = input_file.readline().split()
+
+line = ''
+char_on_line = 0
+
+for word in words:
+    if char_on_line + len(word) <= k:
+        line = line + word + ' '
+        char_on_line = char_on_line + len(word)
+    else:
+        output_file.write(line[:-1] + '\n')
+        line = word + ' '
+        char_on_line = len(word)
+
+output_file.write(line[:-1] + '\n')
+
+input_file.close()
+output_file.close()
